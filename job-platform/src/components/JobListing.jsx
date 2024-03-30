2c7d4af8dc6f2efab4c218c08b2d115fbb23b6f0
Tosin Adewale <tosinadewale01@gmail.com>
2024-03-30 21:16:17 +0000

    Page routing added

--- job-platform/src/components/JobListing.jsx
+++ job-platform/src/components/JobListing.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { FaMapMarker } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const JobListing = ({job}) => {
 
@@ -33,15 +35,15 @@ const JobListing = ({job}) => {
     
                   <div class="flex flex-col lg:flex-row justify-between mb-4">
                     <div class="text-orange-700 mb-3">
-                      <i class="fa-solid fa-location-dot text-lg"></i>
+                      <FaMapMarker className="inline text-lg mb-1 mr-1"/>
                       {job.location}
                     </div>
-                    <a
-                      href={`/job/${job.id}`}
+                    <Link
+                      to={`/job/${job.id}`}
                       class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                      Read More
-                    </a>
+                    </Link>
                   </div>
                 </div>
               </div>
